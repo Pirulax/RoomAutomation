48e2d57fdeba3b16f7164ce714896d697f28da44
Pirulax <patrikjankovics7@gmail.com>
2018-05-12 11:53:27 +0200

    Update WiFiParameters.h

--- WiFiParameters.h
+++ WiFiParameters.h
@@ -6,7 +6,7 @@
 #define AP2_PASSWORD   "********"
 
 /**
- * Credentials of a demo account, you must be change it.
+ * Credentials of a demo account, you must change it to your own accounts credentials before use.
  */
 #define IOT_BASE_URL   "http://api.iothub.live"
 #define IOT_USER_ID    "11cf8f40-460b-11e8-b506-778c45541df0"
